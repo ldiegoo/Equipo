916e6681c827cc1474c0b401bc661daedd407edc
Gibran <l24120331@morelia.tecnm.mx>
2024-09-08 22:12:27 -0600

    Nuevo codigo

--- animalitos.py
+++ animalitos.py
@@ -1,9 +1,7 @@
 animalitos = ("pez", "perro", "tlacuache")
 
 for i in animalitos:
-    if (i != "tlacuache"):
-        print(i + " es un animalito")
-    else:
-        print(i + " es un animalito aplastado")
-    
-    
+        if (i != "tlacuache"):
+                print(i + " es un animalito") 
+        else:
+                print(i + " es un animalito aplastado")
